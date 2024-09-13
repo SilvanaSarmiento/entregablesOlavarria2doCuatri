c20223de2915d142a3cd710bb2d660873494ba1b
SilvanaSarmiento <sil22sarmiento@gmail.com>
2024-09-13 17:17:03 -0300

    corregi y agrege catch de error

--- ejercicioApi/main.js
+++ ejercicioApi/main.js
@@ -1,11 +1,37 @@
-let contenedor = document.getElementById("contenedor");
+const contenedor = document.createElement('div'); //crea el contenedor
+contenedor.classList.add('card'); //para darle clase
+document.body.appendChild(contenedor);
+
 
 fetch("https://jsonplaceholder.typicode.com/users")
-.then(resp => resp.json())
-.then(data => {
-    console.log(data)
-     data.forEach(users => {
-         contenedor.innerHTML += `<h2>Nombre: ${users.name}</h2>
-                                 <h3>Usuario: ${users.username} </h3>
-                                 <h4>Email: ${users.email} </h4>`
-     } )});
+  .then(resp => resp.json())
+  .then(data => {
+    console.log(data);
+   
+    data.forEach(user => {
+    
+    let usuarioDiv = document.createElement('div');
+      
+     let nombre = document.createElement('h2');
+      nombre.textContent = `Nombre: ${user.name}`;
+      
+    
+      let usuario = document.createElement('h3');
+      usuario.textContent = `Usuario: ${user.username}`;
+      
+      let email = document.createElement('h4');
+      email.textContent = `Email: ${user.email}`;
+      
+   
+      usuarioDiv.appendChild(nombre);
+      usuarioDiv.appendChild(usuario);
+      usuarioDiv.appendChild(email);
+      
+   
+      contenedor.appendChild(usuarioDiv);
+    });
+  })
+  .catch(error => {
+    console.log('Error de data:', (error));
+  });
+  
